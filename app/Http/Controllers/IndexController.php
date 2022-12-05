7583fce6797ba37810a840df4c0ee531e8132664
Ronny Wang <ronnywang@gmail.com>
2022-12-05 22:51:47 +0800

    add IndexController

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('index', [
+            'User' => User::class,
+        ]);
+    }
+}
