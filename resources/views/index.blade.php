10544a89e3868dbc7629431cf9ab61fbab666f72
Ronny Wang <ronnywang@gmail.com>
2022-12-05 22:50:16 +0800

    add index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<h3>最新使用者</h3>
+<div class="row">
+@foreach ($User::orderBy('id', 'desc')->take(100)->get() as $user) 
+<?php $data = $user->getData(); ?>
+<div class="card col-2">
+    @isset ($data->avatar)
+    <img class="card-img-top" src="{{ $data->avatar }}" width="260" height="260">
+    @endisset
+    <div class="card-body">
+        <a href="/<?= urlencode($user->name) ?>">
+            <h3 class="card-title">{{ $data->info->name }}</h3>
+            <p>{{ '@' . $user->name }}</p>
+        </a>
+        <p>{{ $data->info->keyword ?? '' }}</p>
+        <p>{{ $data->info->intro ?? '' }}</p>
+    </div>
+</div>
+@endforeach
+</div>
+@endsection
+
