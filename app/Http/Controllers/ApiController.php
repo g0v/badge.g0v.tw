32f36d9a1542ee87fd8462aaaa11ddeec6b4e1f1
Ronny Wang <ronnywang@gmail.com>
2023-02-15 10:51:09 +0800

    add cors_header

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,8 +9,23 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    function cors_header($methods = 'GET')
+    {
+        if (getenv('CORS_Origin')) {
+            foreach (explode(',', getenv('CORS_Origin')) as $origin) {
+                if (array_key_exists('HTTP_ORIGIN', $_SERVER) and $_SERVER['HTTP_ORIGIN'] == $origin) {
+                    header('Access-Control-Allow-Origin: ' . $origin);
+                    header('Access-Control-Allow-Methods: ' . $methods);
+                    header('Access-Control-Allow-Credentials: true');
+                    break;
+                }
+            }
+        }
+    }
     public function me()
     {
+        $this->cors_header();
+
         if (!$user_id = session('user_id')) {
             return ['login' => false];
         }
